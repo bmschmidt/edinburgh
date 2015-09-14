1fd46a12eb96ea07f5f7ea8cff40f8c9f32cc0ec
Benjamin Schmidt <b.schmidt@neu.edu>
2015-09-14 11:17:35 -0400

    Decreasing Verbosity

--- R/bookworm.R
+++ R/bookworm.R
@@ -5,9 +5,11 @@
 ##' to run the query: either by specifying the "query" object the most directly, or
 ##' by filling in the methods one at a time. The latter is easier to understand, but the other
 ##' is sometimes easier when dispatching calls programatically.}
-##' 
+##'
 ##' @title Run a bookworm query
 ##' @param host The domain where the Bookworm lives.
+##' @param port The port that the API lives on; usually 80.
+##' @param database The name of the bookworm to use at the host.
 ##' @param method The Bookworm API method to be used. Only "return_tsv" works particularly
 ##' well at the moment.
 ##' @param search_limits The search constraints, expressed as a list (see below).
@@ -15,30 +17,32 @@
 ##' is applied. (Usually the best choice for non-experts.)
 ##' @param groups A list of groupings to be applied.
 ##' @param counttype A list of summary statistics to calculate.
-##' @param query The Bookworm query to be searched for, expressed as a list. Most of the above methods (query, groups, counttype, aesthetic, etc.)
-##' will update fields in the query, but it can also be called directly. If the query conflicts with any of them (has a different groups element, eg),
-##' currently the query object is the default.          
-##' @param port The port that the API lives on; usually 80.
+##' @param query The Bookworm query to be searched for, expressed as a list. Most of the above methods
+##' (query, groups, counttype, aesthetic, etc.)
+##' will update fields in the query, but it can also be called directly.
+##' If the query conflicts with any of them (has a different groups element, eg),
+##' currently the query object is the default.
+##'
 ##' @return A data.frame consisting of the results of the call.
 ##' @author Benjamin Schmidt
-##' @examples 
-##' 
+##' @examples
+##'
 ##' totals = bookworm(host="bookworm.htrc.illinois.edu", search_limits=list(),
 ##' groups="date_year","counttype"=list("TotalTexts"),
 ##' database="hathipd")
-##' 
+##'
 ##' plot(totals[totals$date_year %in% 1700:2000,],type='l',main="Total works in the Hathi Trust Public Domain corpus")
-##' 
+##'
 ##' totals = bookworm(host="bookworm.htrc.illinois.edu", search_limits=list(),
 ##' groups="date_year","counttype"=list("WordCount","TextCount"),
 ##' database="hathipd")
 ##' totals=totals[totals$date_year %in% 1700:2000,]
 ##' plot(totals$date_year,totals$WordCount/totals$TextCount,type='l',main="Average length of works in the Hathi Trust Public Domain corpus")
-##' 
+##'
 ##' results = bookworm(host="bookworm.htrc.illinois.edu", search_limits=list("word" = list("evolution")),
 ##' groups="date_year","counttype"=list("WordsPerMillion"),
 ##' database="hathipd")
-##' 
+##'
 ##' plot(results[results$date_year %in% 1700:2000,],main="Usage of 'evolution' in the Hathi Trust Public Domain corpus")
 
 
@@ -59,7 +63,7 @@ bookworm = function(
         query[[term]] = get(term)
       }
     }
-    
+
   }
   if (length(query[['search_limits']]) == 0) {query[['search_limits']]=emptyNamedList}
   if (length(query)==0) {
@@ -68,7 +72,7 @@ bookworm = function(
   json = toJSON(query)
   json = URLencode(json)
   destination = paste(host,":",port,"/cgi-bin/dbbindings.py?query=",json,sep="")
-  
+
   if (method!="return_tsv") {
     data = scan(textConnection(getURL(destination)),what='raw',quote=NULL)
     data = paste(data,collapse=" ")
@@ -78,8 +82,6 @@ bookworm = function(
     }
   }
   if (method=="return_tsv") {
-    message(destination)
-    
     text = getURL(destination)
     data = read.table(text=text,
       header=T,
